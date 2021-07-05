7d78eca3caa0f56a86eba938780530383ccc1d6f
WMXPY <wm@mengw.io>
2021-07-05 16:04:30 -0500

    add cli main and optional support

--- src/cli.ts
+++ src/cli.ts
@@ -13,22 +13,32 @@ const coco = Coco.create();
 type LicensePackageCommandOptions = {
 
     readonly targetPath: string;
+
+    readonly main?: string;
+
     readonly dependencies?: string;
     readonly peerDependencies?: string;
+    readonly optionalDependencies?: string;
 };
 
 coco.command(Command.create('license')
     .argument(Argument.create('targetPath'))
+    .option(Option.create('main').optional())
     .option(Option.create('dependencies').optional().boolean())
     .option(Option.create('peerDependencies').optional().boolean())
+    .option(Option.create('optionalDependencies').optional().boolean())
     .then(async (args: LicensePackageCommandOptions) => {
 
         const targetPath: string = Path.resolve(args.targetPath);
         await licensePackage({
 
             targetPath,
+
+            main: args.main ?? 'index.js',
+
             dependencies: args.dependencies === 'true',
             peerDependencies: args.peerDependencies === 'true',
+            optionalDependencies: args.optionalDependencies === 'true',
         });
     }),
 );
